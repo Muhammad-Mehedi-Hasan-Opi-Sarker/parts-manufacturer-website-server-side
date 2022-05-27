b59e19db82b8e0a1d36e56180f1576f10e0ac216
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-05-27 12:01:17 +0600

    add connected mongodb

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = "mongodb+srv://parts_admin:<password>@cluster0.mswof.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    console.log('connected')
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 app.get('/', (req, res) => {
   res.send(' parts site is running')
