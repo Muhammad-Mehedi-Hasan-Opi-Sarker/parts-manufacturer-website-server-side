0e15456d520f66af2fa8aeb94dfc87dc509c6bea
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-05-27 17:11:50 +0600

    database from server

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 5000;
+
 
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://parts_admin:<password>@cluster0.mswof.mongodb.net/?retryWrites=true&w=majority";
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mswof.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    console.log('connected')
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("parts_manufacturer").collection("products");
+
+    app.get('/product', async(req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    })
+
+  } finally {
+
+  }
+}
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
   res.send(' parts site is running')
